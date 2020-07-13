c54dab05d79e8e66e4be4c52394980a52ebeb8f8
Adrien <adrien.godaert@softedge.be>
2020-07-13 15:39:25 +0200

    update migration

--- migrations/m200712_133200_create_location_municipalities_table.php
+++ migrations/m200712_133200_create_location_municipalities_table.php
@@ -7,7 +7,7 @@ class m200710_093600_create_shop_postal_code_table extends Migration
 
     public function up()
     {
-        $this->createTable("{{%location_municipalities}}",
+        $this->createTable("{{%shop_postal_code}}",
             [
                 'zip' => $this->integer()->notNull(),
                 'name' => $this->string(255)->defaultValue(null),
@@ -16,6 +16,21 @@ class m200710_093600_create_shop_postal_code_table extends Migration
             ],
             'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB'
         );
+
+        $municipalities = require(__DIR__ . '/data/municipalities_location.php');
+        try {
+            foreach ($municipalities as $municipality) {
+                $this->insert('{{%shop_postal_code}}', [
+                    'zip' => $municipality[0],
+                    'name' => $municipality[1],
+                    'longitude' => $municipality[2],
+                    'latitude' => $municipality[3],
+                ]);
+            }
+
+        } catch (\Exception $e) {
+            echo print_r($e->getMessage());
+        }
     }
 
     public function down()
